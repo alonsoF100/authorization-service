6df8f97f4e9311d6312df327931005e38a6c6525
alonsoF100 <yanul228@mail.ru>
2025-12-16 17:21:24 +0300

    add some logging

--- internal/repository/postgres/database.go
+++ internal/repository/postgres/database.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"context"
+	"log/slog"
 
 	"github.com/alonsoF100/authorization-service/internal/config"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -20,8 +21,14 @@ func New(pool *pgxpool.Pool) *Repository {
 }
 
 func NewPool(cfg *config.Config) (*pgxpool.Pool, error) {
+	const pp = "internal/repository/postgres/database.go/NewPool"
+
 	poolConfig, err := pgxpool.ParseConfig(cfg.Database.ConStr())
 	if err != nil {
+		slog.Error("Failed to create pgx pool cfg",
+			"Path", pp,
+			"Error", err,
+		)
 		return nil, err
 	}
 
@@ -29,17 +36,29 @@ func NewPool(cfg *config.Config) (*pgxpool.Pool, error) {
 
 	pool, err := pgxpool.NewWithConfig(context.Background(), poolConfig)
 	if err != nil {
+		slog.Error("Failed to create pgx pool",
+			"Path", pp,
+			"Error", err,
+		)
 		return nil, err
 	}
 
 	err = pool.Ping(context.Background())
 	if err != nil {
+		slog.Error("Failed to ping database",
+			"Path", pp,
+			"Error", err,
+		)
 		return nil, err
 	}
 
 	connConfig := poolConfig.ConnConfig
 	db := stdlib.OpenDB(*connConfig)
 	if err := goose.Up(db, cfg.Migration.Dir); err != nil {
+		slog.Error("Failed to UP migrate database",
+			"Path", pp,
+			"Error", err,
+		)
 		return nil, err
 	}
 
