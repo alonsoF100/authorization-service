1440a3f7490779f6d0dc7a86fb65ad31c67b3766
alonsoF100 <yanul228@mail.ru>
2025-12-16 13:01:29 +0300

    add slog logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,9 +4,12 @@ import (
 	"log"
 
 	"github.com/alonsoF100/authorization-service/internal/config"
+	"github.com/alonsoF100/authorization-service/internal/logger"
 )
 
 func main() {
 	cfg := config.Load()
 	log.Println(*cfg)
+
+	logger.Setup(cfg)
 }
