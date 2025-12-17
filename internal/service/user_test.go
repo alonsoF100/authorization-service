9b23eb1f993478ae893de0b4d90bc5520d3ea5a5
alonsoF100 <yanul228@mail.ru>
2025-12-18 01:03:16 +0300

    add tests to service.GetUser

--- internal/service/user_test.go
+++ internal/service/user_test.go
@@ -2,23 +2,25 @@ package service_test
 
 import (
 	"context"
+	"errors"
 	"testing"
 
+	"github.com/alonsoF100/authorization-service/internal/apperrors"
 	"github.com/alonsoF100/authorization-service/internal/models"
 	"github.com/alonsoF100/authorization-service/internal/service"
-	"github.com/alonsoF100/authorization-service/mocks"
 	"github.com/gojuno/minimock/v3"
-	"github.com/stretchr/testify/assert"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetUserSuccess(t *testing.T) {
 	mc := minimock.NewController(t)
-	mockRepo := mocks.NewUserRepositoryMock(mc)
+	mockRepo := service.NewUserRepositoryMock(mc)
 
 	ctx := context.Background()
-	userID := "123"
+	userID := uuid.New().String()
 	expectedUser := &models.User{
-		ID:       "123",
+		ID:       userID,
 		Nickname: "John Doe",
 		Email:    "john@example.com",
 	}
@@ -29,7 +31,45 @@ func TestGetUserSuccess(t *testing.T) {
 
 	user, err := userService.GetUser(ctx, userID)
 
-	assert.NoError(t, err)
-	assert.NotNil(t, user)
-	assert.Equal(t, expectedUser, user)
+	require.NoError(t, err)
+	require.NotNil(t, user)
+	require.Equal(t, expectedUser, user)
+}
+
+func TestGetUserDatabaseError(t *testing.T) {
+	mc := minimock.NewController(t)
+	mockRepo := service.NewUserRepositoryMock(mc)
+
+	ctx := context.Background()
+	userID := uuid.New().String()
+	someErr := errors.New("database error")
+
+	mockRepo.FindByIDMock.Expect(ctx, userID).Return(nil, someErr)
+
+	userService := service.NewUserService(mockRepo)
+
+	user, err := userService.GetUser(ctx, userID)
+
+	require.Error(t, err)
+	require.True(t, errors.Is(err, someErr))
+	require.Nil(t, user)
+}
+
+func TestGetUserNotFound(t *testing.T) {
+	mc := minimock.NewController(t)
+	mockRepo := service.NewUserRepositoryMock(mc)
+
+	ctx := context.Background()
+	userID := uuid.New().String()
+	someErr := apperrors.ErrUserNotFoundByID
+
+	mockRepo.FindByIDMock.Expect(ctx, userID).Return(nil, nil)
+
+	userService := service.NewUserService(mockRepo)
+
+	user, err := userService.GetUser(ctx, userID)
+
+	require.Error(t, err)
+	require.Equal(t, someErr, err)
+	require.Nil(t, user)
 }
